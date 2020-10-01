972f838f8a95a12e53ad0b211d9b4a702f68acb8
Alujixixi <yan.adol9812@gmail.com>
2020-10-01 23:15:50 +0800

    first commit!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainPage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from "./components/MainPage";
+import './styles/buttons.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPage,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body {
+    position: fixed;
+    width: 100% !important;
+    height: 100% !important;
+    margin: 0      !important;
+    padding: 0    !important;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 0 auto;
+    padding: 0;
+    display: flex;
+    justify-content: center; /*定义body的里的元素水平居中*/
+    position: relative;
+    height: 100%;
+
+  }
 </style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,90 @@
+<template>
+    <div id="main_div">
+        <p id="main_name">
+            {{who_am_i}}
+        </p>
+        <a class="button button-glow button-rounded button-raised button-primary"
+            @click="gogogo">奥力给!</a>
+<!--        <a class="button button-glow button-border button-rounded button-primary">奥利给!</a>-->
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "MainPage",
+        data: function() {
+        return {
+                who_am_i: "Press “奥力给！” to Start",
+                the_list: [
+                    "唐少",
+                    "GC",
+                    "三七开",
+                    "冷冷",
+                    "臭猪1",
+                    "臭猪2",
+                    "臭猪3",
+                    "臭猪4",
+                    "臭猪5",
+                    "臭猪6",
+                    "臭猪7",
+                    "臭猪8",
+                ]
+            };
+        },
+        computed: {
+        },
+        methods: {
+            sleep: function(d){
+                for(let t = Date.now();Date.now() - t <= d;);
+            },
+            gogogo: function () {
+                this.start_choosing();
+            },
+            start_choosing: function () {
+                let n = this.the_list.length;
+                for(let i = 0; i < 3*n; i++) {
+                    let time = 100 * i;
+                    setTimeout( this.choosing, time);
+                    console.log(time);
+                    // this.func();
+                    // let idx = Math.floor(Math.random()*this.the_list.length);
+                    // this.who_am_i = this.the_list[idx];
+                    //
+                    // console.log(this.who_am_i);
+                    // // if(Math.abs(i-n) > 5)
+                    // this.sleep(100);
+                }
+            },
+            func: function() {
+                let idx = Math.floor(Math.random()*this.the_list.length);
+                this.who_am_i = this.the_list[idx];
+
+                console.log(this.who_am_i);
+                // if(Math.abs(i-n) > 5)
+                this.sleep(100);
+            },
+            choosing: function () {
+                let idx = Math.floor(Math.random()*this.the_list.length);
+                this.who_am_i = this.the_list[idx];
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #main_name {
+        font-size: 10vw;
+    }
+
+    #main_div {
+        margin: 0 auto;
+        top: 25%;
+        position: relative;
+    }
+
+    #start_btn {
+
+    }
+
+</style>
